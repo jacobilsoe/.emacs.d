257b336cb9100a05b1c56cad066a1af21c11bced
Jacob Ilsø <jacobilsoe@gmail.com>
2015-12-15 22:06:04 +0100

    Added auto-package-update

--- jacobilsoe/jacobilsoe-packages.el
+++ jacobilsoe/jacobilsoe-packages.el
@@ -9,4 +9,10 @@
 
 (package-refresh-contents)
 
+(require-package 'auto-package-update)
+(setq auto-package-update-delete-old-versions t)
+(auto-package-update-maybe)
+(setq auto-package-update-interval 1)
+(auto-package-update-at-time "05:00")
+
 (provide 'jacobilsoe-packages)
