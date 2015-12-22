a5cbe8a94089cf575ac3afb1bdb604158ab5c487
Jacob Ilsø <jacobilsoe@gmail.com>
2015-12-22 17:21:52 +0100

    Dired details tweaks

--- jacobilsoe/jacobilsoe-dired.el
+++ jacobilsoe/jacobilsoe-dired.el
@@ -16,4 +16,34 @@
 
 (diredp-toggle-find-file-reuse-dir 1)
 
+(setq ls-lisp-dirs-first t)
+(setq ls-lisp-verbosity nil)
+
+(defun group-number (num &optional size char)
+  "Format NUM as string grouped to SIZE with CHAR."
+  ;; Based on code for `math-group-float' in calc-ext.el
+  (let* ((size (or size 3))
+	 (char (or char ","))
+	 (str (if (stringp num)
+		  num
+		(number-to-string num)))
+	 ;; omitting any trailing non-digit chars
+	 ;; NOTE: Calc supports BASE up to 36 (26 letters and 10 digits ;)
+	 (pt (or (string-match "[^0-9a-zA-Z]" str) (length str))))
+    (while (> pt size)
+      (setq str (concat (substring str 0 (- pt size))
+			char
+			(substring str (- pt size)))
+	    pt (- pt size)))
+    str))
+
+(defun ls-lisp-format-file-size (file-size human-readable)
+  (format "%15s" (group-number file-size 3 ".")))
+
+(defadvice ls-lisp-format (around my-ls-lisp-format (file-name file-attr file-size switches time-index))
+  (progn
+    ad-do-it
+    (setq ad-return-value (substring ad-return-value 10))))
+(ad-activate 'ls-lisp-format t)
+
 (provide 'jacobilsoe-dired)
