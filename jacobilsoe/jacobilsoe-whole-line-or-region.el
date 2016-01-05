fe47777a211d594618c8650b4679caca5c22b39e
Jacob Ilsø <jacobilsoe@gmail.com>
2016-01-05 09:55:46 +0100

    Added comment-dwim to whole-line-or-region

--- jacobilsoe/jacobilsoe-whole-line-or-region.el
+++ jacobilsoe/jacobilsoe-whole-line-or-region.el
@@ -1,5 +1,7 @@
 (require-package 'whole-line-or-region)
 
+(require 'whole-line-or-region)
+(add-to-list 'whole-line-or-region-extensions-alist '(comment-dwim whole-line-or-region-comment-dwim nil))
 (whole-line-or-region-mode t)
 
 (provide 'jacobilsoe-whole-line-or-region)
