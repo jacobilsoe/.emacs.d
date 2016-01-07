ed27f076130e07621a60be39b3a4167bb2f79923
Jacob Ilsø <jacobilsoe@gmail.com>
2016-01-07 22:15:59 +0100

    Use zero padded month and day

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
 
 (load-theme 'wombat t)
 
-(setq display-time-string-forms '((propertize (concat day "/" month "/" year " " 24-hours ":" minutes) 'face 'font-lock-string-face)))
+(setq display-time-string-forms '((propertize (format-time-string "%d/%m/%Y %H:%M" now) 'face 'font-lock-string-face)))
 (display-time-mode)
 (column-number-mode)
 
