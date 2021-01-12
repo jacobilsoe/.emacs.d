fe2913be9917614e1c2184b1e2e4d9411f4f96fc
Jacob Ilsø <jacobilsoe@gmail.com>
2021-01-12 20:17:37 +0100

    Stop using dired+ and ls-lisp

--- init.el
+++ init.el
@@ -219,8 +219,8 @@
 
 (use-package all-the-icons)
 (use-package all-the-icons-dired
-  :init
-  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+  :hook
+  (dired-mode . all-the-icons-dired-mode))
 
 ;;; org
 
@@ -235,91 +235,33 @@
 
 ;;; dired
 
-(use-package dired+
-  :load-path "packages/")
+(use-package dired
+  :hook
+  (dired-mode . dired-hide-details-mode)
+  (dired-mode . hl-line-mode)
+  (dired-mode . auto-revert-mode)
+  :config
+  (setq dired-listing-switches "-agho --group-directories-first --time-style=long-iso")
+  (setq auto-revert-verbose nil)
+  (setq auto-revert-interval 1))
 
+;; Use dired-narrow
 (use-package dired-narrow)
-
-(require 'dired+)
-
-;; Enable ls-lisp
-(require 'ls-lisp)
-(setq ls-lisp-use-insert-directory-program nil)
-
-;; Key bindings
-(define-key dired-mode-map (kbd "C-<up>") 'diredp-up-directory)
 (define-key dired-mode-map (kbd "C-n") 'dired-narrow)
-
-;; Show active line
-(add-hook 'dired-mode-hook 'hl-line-mode)
-
-;; Only use a single buffer for dired
-(diredp-toggle-find-file-reuse-dir 1)
-
-;; Different ls-lisp formatting options
-(setq ls-lisp-dirs-first t)
-(setq ls-lisp-verbosity nil)
-(setq ls-lisp-ignore-case t)
-
-;; Use thousand separator in file size
-(defun group-number (num &optional size char)
-  "Format NUM as string grouped to SIZE with CHAR."
-  ;; Based on code for `math-group-float' in calc-ext.el
-  (let* ((size (or size 3))
-	 (char (or char ","))
-	 (str (if (stringp num)
-		  num
-		(number-to-string num)))
-	 ;; omitting any trailing non-digit chars
-	 ;; NOTE: Calc supports BASE up to 36 (26 letters and 10 digits ;)
-	 (pt (or (string-match "[^0-9a-zA-Z]" str) (length str))))
-    (while (> pt size)
-      (setq str (concat (substring str 0 (- pt size))
-			char
-			(substring str (- pt size)))
-	    pt (- pt size)))
-    str))
-
-(defun ls-lisp-format-file-size (file-size human-readable)
-  (format "%15s" (group-number file-size 3 ".")))
-
-(defun my-ls-lisp-format (res)
-  (let ((dirstring (if (string= "d" (substring res 0 1)) "d" " ")))
-    (concat dirstring (substring res 10))))
-(advice-add 'ls-lisp-format :filter-return #'my-ls-lisp-format)
-
-(defun my-insert-directory(FILE SWITCHES &optional WILDCARD FULL-DIRECTORY-P)
-  (save-excursion
-    (save-match-data
-      (goto-char (point-min))
-      (when (re-search-forward "^ *total used in directory \\([0-9]+\\) available \\([0-9]+\\)")
- 	(replace-match (format "%s k" (group-number (match-string 1) 3 ".")) nil nil nil 1)
- 	(replace-match (format "%s k" (group-number (match-string 2) 3 ".")) nil nil nil 2)
- 	))))
-(advice-add 'insert-directory :after #'my-insert-directory)
 
 ;; Ensure jumping to beginning and end of buffer stays within file list
 (defun my-dired-jump-to-first-entry ()
   (interactive)
   (beginning-of-buffer)
   (dired-goto-next-nontrivial-file))
-
-(define-key dired-mode-map
-  (vector 'remap 'beginning-of-buffer) 'my-dired-jump-to-first-entry)
-
+(define-key dired-mode-map [remap beginning-of-buffer] 'my-dired-jump-to-first-entry)
 (defun my-dired-jump-to-last-entry ()
   (interactive)
   (end-of-buffer)
   (dired-next-line -1))
+(define-key dired-mode-map [remap end-of-buffer] 'my-dired-jump-to-last-entry)
 
-(define-key dired-mode-map
-  (vector 'remap 'end-of-buffer) 'my-dired-jump-to-last-entry)
-
-;; Use custom format for time
-(setq ls-lisp-format-time-list  '("%Y-%m-%d %H:%M" "%Y-%m-%d %H:%M"))
-(setq ls-lisp-use-localized-time-format t)
-
-;; Ensure external changes are reflected in dired
-(add-hook 'dired-mode-hook 'auto-revert-mode)
-(setq auto-revert-verbose nil)
-(setq auto-revert-interval 1)
+;; Ensure only a single dired buffer is used
+(use-package dired-single)
+(define-key dired-mode-map [remap dired-find-file] 'dired-single-buffer)
+(define-key dired-mode-map [remap dired-up-directory] 'dired-single-up-directory)
