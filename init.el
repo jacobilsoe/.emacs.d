86c18a49149d0c6692a30be2ba06254354eacd5c
Jacob Ilsø <jacobilsoe@gmail.com>
2021-11-25 18:25:35 +0100

    Changed key binding to find files

--- init.el
+++ init.el
@@ -351,9 +351,9 @@
 (defhydra hydra-dired (:color pink :hint nil)
 "
 ^Mark^                  ^Operate^                       ^View^                              ^Search^
-^^^^^^^^---------------------------------------------------------------------------------------------------------
-_m_   : mark            _C_       : copy                _g_       : refresh                 _f f_ : find files
-_% m_ : mark regexp     _% C_     : copy regexp         _(_       : toggle details          _A_   : find in files
+^^^^^^^^-------------------------------------------------------------------------------------------------------------
+_m_   : mark            _C_       : copy                _g_       : refresh                 _M-s M-d_ : find files
+_% m_ : mark regexp     _% C_     : copy regexp         _(_       : toggle details          _A_       : find in files
 _% g_ : mark containing _R_       : rename/move         _C-n_     : narrow
 _u_   : unmark          _% R_     : rename/move regexp  _s_       : toggle sorting
 _U_   : unmark all      _D_       : delete              _v_       : view file
@@ -386,7 +386,7 @@ _t_   : toogle marks    _Z_       : compress/uncompress _M-s M-s_ : show total s
 ("C-x C-q" dired-toggle-read-only)
 ("w" dired-copy-filename-as-kill)
 ("C-0 w" (lambda () (interactive) (let ((current-prefix-arg 0)) (call-interactively #'dired-copy-filename-as-kill))))
-("f f" find-name-dired)
+("M-s M-d" find-name-dired)
 ("A" dired-do-find-regexp)
 ("q" quit-window "quit" :color blue)
 ("?" nil :color blue))
@@ -410,6 +410,7 @@ _t_   : toogle marks    _Z_       : compress/uncompress _M-s M-s_ : show total s
 	      ([remap end-of-buffer] . 'ji/dired-jump-to-last-entry)
 	      ("M-RET" . 'ji/dired-open-file-in-external-app)
 	      ("M-s M-s" . 'ji/dired-get-size)
+	      ("M-s M-d" . 'find-name-dired)
 	      ("=" . 'ji/ediff-marked-pair)
 	      ("?" . 'hydra-dired/body)))
 
