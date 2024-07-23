51c44a092d3eb637a04df248d95c0aff77e4375b
Jacob Ilsø <jacobilsoe@gmail.com>
2024-07-23 16:55:53 +0200

    Do not override file completion

--- init.el
+++ init.el
@@ -344,8 +344,7 @@
 (use-package orderless
   :init
   (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
+        completion-category-defaults nil))
 
 (use-package consult
   :bind
